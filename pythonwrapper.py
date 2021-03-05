ad6618324b4ac5ef72fba0abea17f2425c2d0755
Genevieve Baddoo <gennynat@gmail.com>
2021-03-04 21:24:50 -0600

    update wrapper

--- pythonwrapper.py
+++ pythonwrapper.py
@@ -1,51 +1,28 @@
 #import necessary modules for HCMV project
 import os
-import glob 
 import argparse
 from Bio import SeqIO
 from Bio import Entrez
-
-
-
-
+from Bio import SearchIO
+from Bio.Seq import Seq
+from Bio.Blast import NCBIWWW
 
 #Define a function for each tool/step in wrapper 
 
 #Step 1 
 
 #Convert SRA files from NCBI to fastq paired-end reads. Construct path based on SRR numbers
-mainoutputfile = open("miniProject.log", "w+") #produce output file requested called miniProject.log for whole wrapper
-path = os.getcwd() #returns current working directory of all code processes
+#defined SSRs as a list
+SRR = ['SRR5660030', 'SRR5660033', 'SRR5660044', 'SRR5660045']
+#print(SRR)
 
+def fastq(SRR):
+  wget = 'wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/' + SRR + '/' + SRR + '.1'
+  fastq = 'fastq-dump -I --split-files '+ str(SRR) #what you would use in terminal command line
+  rename = 'mv '+ SRR + '.1 ' + SRR
+  os.system(wget) #os.system runs the command line
+  os.system(fastq)
+  os.system(rename)
 
-def get_ncbi_data(SRR):
-    wget = 'wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/' + SRR + '/' + SRR + '.1'
-    rename = 'mv ' + SRR + '.1 ' + SRR
-    os.system(wget)
-    os.system(rename)
-
-
-def run_fastq(SRR):
-    fastqfiles = 'fastq-dump -I --split-files ' + SRR + '.1'
-    os.system(fastqfiles)
-    print(fastqfiles)
-
-
-
-
-
-
-
-
-
-SRR = []
-
-
-
-
-  
-
-
-get_ncbi_data(SRR)
-run_fastq(SRR)
-
+for i in SRR:
+  fastq(i)
