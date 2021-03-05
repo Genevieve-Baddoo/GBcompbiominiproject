74f5a6f365e432f4d98b7a3cea9625765caf2de3
Genevieve Baddoo <gennynat@gmail.com>
2021-03-04 21:07:40 -0600

    update wrapper

--- pythonwrapper.py
+++ pythonwrapper.py
@@ -33,20 +33,12 @@ def run_fastq(SRR):
 
 
 
-#want to parse
-parser = argparse.ArgumentParser()
-parser.add_argument("--SRR", nargs="+", required=True, help = "SRR input to assemble and BLAST")
-parser.add_argument("--fastq", action="store_true", help = "SRR input data in paired end fastq format")
-p = parser.parse_args()
 
 
 
 
 
 SRR = []
-#get SRRs from argument
-for SRR in p.SRR:
-  SRR.append(SRR)
 
 
 
