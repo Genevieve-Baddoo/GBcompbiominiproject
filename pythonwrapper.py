fd35b9623411b7f86553b06b2b8fe786f8275943
Genevieve Baddoo <gennynat@gmail.com>
2021-03-05 09:13:48 -0600

    add kallist

--- pythonwrapper.py
+++ pythonwrapper.py
@@ -28,7 +28,41 @@ def fasta():
     getfasta = '/usr/bin/python3 miniproject2.py'
     os.system(getfasta)
 
+#Step 3
 
+#define function to take SRR # and create/run Kallisto on the command line
+#use CDS fasta file created in the previous py code
+def kallisto_idx():
+  run_idx = "kallisto index -i hcmv_index.idx EF999921_CDS.fasta"
+  os.system(run_idx)
+
+
+
+path = os.getcwd()
+
+def kallisto_quant(SRRs):
+  for SRR in SRRs:
+    quant = 'time kallisto quant -i HCMV_index.idx -o' + path + '/results_' + SRR + ' -b 30 -t 4 ' + SRR + '.1_1.fastq ' + SRR + '.1_2.fastq'
+    os.system(quant)
+
+
+#define function to generate the sleuth input
+def sleuth_input(SRRs):
+    # output file that goes in R
+    output = open('sleuth_infile.txt', 'w')
+    # initial line in file
+    output.write('sample' + '\t' + 'condition' + '\t' + 'path' + '\n')
+    # based on SRR number, write condition and path to output file
+    path = os.getcwd()
+    for SRR in SRRs:
+        path1 = path + '/' + 'results_' + SRR
+        print(path1)
+        if int(SRR[
+               3:]) % 2 == 0:  
+            output.write(str(SRR) + '\t' + '2dpi' + '\t' + path1 + '\n')
+        else:
+            output.write(str(SRR) + '\t' + '6dpi' + '\t' + path1 + '\n')
+    output.close()
 
 
 
@@ -39,6 +73,9 @@ def fasta():
 
 
 #run functions
-
 fastq(SRRs)
 fasta()
+kallisto_idx()
+kallisto_quant(SRRs)
+sleuth_input(SRRs)
+
