a8298c006dd17e4b97adc293fd0e8d17975ed125
Genevieve Baddoo <gennynat@gmail.com>
2021-03-06 07:11:12 -0600

    added most functions

--- pythonwrapper.py
+++ pythonwrapper.py
@@ -76,6 +76,7 @@ def sleuth():
         log_file.write(SRR + '\n')
 
 
+#Step 4
 
 #function to run bowtie2
 def bowtie2(SRRs):
@@ -86,34 +87,35 @@ def bowtie2(SRRs):
     run_bowtie2 = "bowtie2 --no-unal --al-conc " + SRR + " --quiet -x HCMV -1 "+ SRR +".1_1.fastq -2 " + SRR + ".1_2.fastq -S " + SRR + "map.sam"
     os.system(run_bowtie2)
 
-
-#define function to count reads before and after filtering with bowtie2
+#find the number of reads in each transcriptome before and after the Bowtie2 mapping
 def num_reads(SRRs):
-  log_file = open("miniProject.log", "a")
-  for SRR in SRRs:
-    name1 = "mv " + SRR + ".1_1." + SRR + ".1.fastq"
-    os.system(name1) #rename bowtie2 output files to end in fastq
-    name2 = "mv " + SRR + ".1_2." + SRR + ".2.fastq"
-    os.system(name2) #rename bowtie2 output files to end in fastq
-    fastq_before = open(SRR + ".1_1.fastq")
-    fastq_after = open(SRR + ".1.fastq")
-    count_before = 0
-    count_after = 0
-    for line in fastq_before:
-      count_before = count_before+1
-    for line in fastq_after:
-      count_after = count_after+1
-    count_before = int(count_before/4)
-    count_after = int(count_after/4)
-    if SRR == SRR[0]:
-      log_file.write("Donor 1 (2dpi) had " + str(count_before) + " read pairs before Bowtie2 filtering and " + str(count_after) + " read pairs after.\n")
-    elif SRR == SRR[1]:
-      log_file.write("Donor 1 (6dpi) had " + str(count_before) + " read pairs before Bowtie2 filtering and " + str(count_after) + " read pairs after.\n")
-    elif SRR == SRR[2]:
-      log_file.write("Donor 3 (2dpi) had " + str(count_before) + " read pairs before Bowtie2 filtering and " + str(count_after) + " read pairs after.\n")
-    elif SRR == SRR[3]:
-      log_file.write("Donor 3 (6dpi) had " + str(count_before) + " read pairs before Bowtie2 filtering and " + str(count_after) + " read pairs after.\n")    
-  log_file.close()
+    for i in range(0,len(SRRs)):
+        if i == 0:
+            donor = "Donor 1 (2dpi)"
+        elif i == 1:
+            donor = "Donor 1 (6dpi)"
+        elif i == 2:
+            donor = "Donor 3 (2dpi)"
+        elif i ==3:
+            donor = "Donor 3 (6dpi)"
+
+        beforebow_1 = open(str(SRRs[i])+'.1_1.fastq').readlines()
+        beforebow_2 = open(str(SRRs[i])+'.1_2.fastq').readlines()
+        before_reads = (len(beforebow_1) + len(beforebow_2))/8
+
+        afterbow_1 = open('Bowtie2_'+ str(SRRs[i])+'.1.fastq').readlines()
+        afterbow_2 = open('Bowtie2_'+ str(SRRs[i])+'.2.fastq').readlines()
+        after_reads = (len(afterbow_1) + len(afterbow_2))/8
+
+
+        log_file = open("miniProject.log", "a")
+        log_file.write(donor + "had " + str(before_reads) + " read pairs before Bowtie2 filtering and " + str(after_reads) + " read pairs after." + "\n")
+        log_file.close()
+
+
+#Step 5
+#define function to run spades
+
 
 def run_spades(SRRs):
     spades = 'spades -k 55,77,99,127 -t 2 --only-assembler --pe1-1 ' + SRRs[0] + '.1.fastq --pe1-2 '+ SRRs[0] + '.2.fastq --pe2-1 ' + SRRs[1] + '.1.fastq --pe2-2 ' + SRRs[1] + '.2.fastq --pe3-1 ' + SRRs[2] + '.1.fastq --pe3-2 ' + SRRs[2] +'.2.fastq --pe4-1 ' + SRRs[3] + '.1.fastq --pe4-2 ' + SRRs[3] + '.2.fastq -o spades'
@@ -123,6 +125,39 @@ def run_spades(SRRs):
     miniProject_log.close()
 
 
+#Step 6
+
+#function to conunt and subset contigs longer than 1000 bp  
+def contig_subset():
+  subset = "python3 miniproject6.py" #python code in repo
+  os.system(subset)
+
+
+#Step 7
+
+#function to calculate assembly length  
+def length_assembly():
+  calc = "python3 miniproject7.py" #python code in repo
+  os.system(calc)
+
+
+#Step 8
+  
+#function to run blast using the largest contig  
+#make sure to blast against a local database
+def blast_longestcontigs():
+    blast_input = '/sequence.fasta'
+    makeblastdb_command = 'makeblastdb -in ' + path + blast_input +' -out ' + path + '/betaherpesvirinae -title betaherpesvirinae -dbtype nucl'
+    print(path + input_blast)
+    os.system(makeblastdb_command)
+
+    blastn_command = 'blastn -query ' + path + '/longest_contig.fasta -db '+ path + 'betaherpesvirinae -max_target_seqs 10 -out ' + path + '/blast_results.txt -outfmt "6 sacc pident length qstart qend sstart send bitscore evalue stitle"'
+    os.system(blastn_command)
+    log_file.write('sacc' + '\t' + 'pident' + '\t' + 'length' + '\t' + 'qstart' + '\t' + 'qend' + '\t' + 'sstart' + '\t' + 'send' + '\t' + 'bitscore' + '\t' + 'eval' + '\t' + 'stitle' + '\n') #write to log
+    read_blast_results = open(path + 'blast_results.txt', 'r').readlines()
+    for i in read_blast_results:
+        log_file.write(str(i))
+        log_file.write('\n')
 
 
 
@@ -154,3 +189,6 @@ sleuth()
 bowtie2(SRRs)
 num_reads(SRRs)
 run_spades(SRRs)
+contig_subset()
+length_assembly()
+blast_longestcontigs()
