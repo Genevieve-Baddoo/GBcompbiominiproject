52b81ac589874dabfbaa22582eae82bf4a9769df
Genevieve Baddoo <gennynat@gmail.com>
2021-03-06 01:20:39 -0600

    edit spaded command

--- pythonwrapper.py
+++ pythonwrapper.py
@@ -116,7 +116,7 @@ def num_reads(SRRs):
   log_file.close()
 
 def run_spades(SRRs):
-    spades = 'spades -k 55,77,99,127 -t 2 --only-assembler --pe1-1 ' + SRRs[0] + '.1.fastq --pe1-2 '+ SRRs[0] + '.2.fastq --pe2-1 ' + SRRs[1] + '.1.fastq --pe2-2 ' + SRRs[1] + '.2.fastq --pe3-1 ' + SRRs[2] + '.1.fastq --pe3-2 ' + SRRs[2] +'.2.fastq --pe4-1 ' + SRRs[3] + '.1.fastq --pe4-2 ' + SRRs[3] + '.2.fastq' -o spades’
+    spades = 'spades -k 55,77,99,127 -t 2 --only-assembler --pe1-1 ' + SRRs[0] + '.1.fastq --pe1-2 '+ SRRs[0] + '.2.fastq --pe2-1 ' + SRRs[1] + '.1.fastq --pe2-2 ' + SRRs[1] + '.2.fastq --pe3-1 ' + SRRs[2] + '.1.fastq --pe3-2 ' + SRRs[2] +'.2.fastq --pe4-1 ' + SRRs[3] + '.1.fastq --pe4-2 ' + SRRs[3] + '.2.fastq -o spades'
     os.system(spades)
     miniProject_log = open("miniProject.log", "a")
     miniProject_log.write(spades + '\n')
